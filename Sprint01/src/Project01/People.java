fbc48df2bb4da4bb90343b6c6fb5d51368f2ecbf
Orlando <rodriguezoliverao@moravian.edu>
2023-02-07 10:58:38 -0500

    People described. Documentation done.

--- Sprint01/src/Project01/People.java
+++ Sprint01/src/Project01/People.java
@@ -2,6 +2,10 @@ package Project01;
 
 import Project01.PeopleType;
 
+/**
+ *Stores information pertaining to the people of a nation/tribes. Things such as their nation, tribe, description,
+ * etc. The values are private and cannot be accessed directly.
+ */
 public class People
 {
     private String personName;
@@ -12,6 +16,14 @@ public class People
     private int myLifePoints;
 
 
+    /**
+     * The Constructor for 'People'
+     * Makes the people for each tribe/nation using the passed parameters, nation, tribe, person and the assigned life points.
+     * @param nation
+     * @param tribe
+     * @param person
+     * @param lifePoints
+     */
     public People(String nation, String tribe, PeopleType person, int lifePoints)
     {
         myNation = nation;
@@ -21,31 +33,57 @@ public class People
         myLifePoints = lifePoints;
     }
 
+    /**
+     * Returns the type of individual(wizard/warrior) that said person is, callable from public code.
+     * @return
+     */
     public PeopleType getType()
     {
         return me;
     }
 
+    /**
+     * Returns what tribe the individual is from, callable in public code.
+     * @return
+     */
     public String getTribe()
     {
         return myTribe;
     }
 
+    /**
+     * Returns which nation the individual is from, callable in public code
+     * @return
+     */
     public String getNation()
     {
         return myNation;
     }
 
+    /**
+     * A check if the person is still alive by returning a true or false based on if the life points are above 0,
+     * callable from public.
+     * @return
+     */
     public Boolean isPersonAlive()
     {
         return (myLifePoints > 0);
     }
 
+    /**
+     * Returns specifically how many life points the person has, callable from public code.
+     * @return
+     */
     public int getLifePoints()
     {
         return myLifePoints;
     }
 
+    /**
+     * It doesn't do anything. Everything is commented out. Why is this here tho?
+     * Would normally take care of what would happen when meeting another person. Peaceful or violent.
+     * @param otherPerson
+     */
     public void encounterStrategy(People otherPerson)
     {
         if(myNation == otherPerson.getNation())
@@ -62,12 +100,20 @@ public class People
     }
 
 
+    /**
+     * A public functions which takes care of reducing a person's life points as they take damage.
+     * @param points
+     */
     public void reduceLifePoints(int points)
     {
         myLifePoints = myLifePoints - points;
     }
 
 
+    /**
+     * Returns a string wit the person's information.
+     * @return
+     */
     public String toString()
     {
         String result = new String( myNation + "\t" +  myTribe + "\t" + me + "\t" + myDescription + "\t" + myLifePoints);
